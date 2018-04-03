a6489b377d7451b29c62a80e7f57d7b576513969
Tim Schubert <tim.schubert@tu-bs.de>
2018-04-03 16:48:41 +0200

    Add JavaDoc

--- TestFile.java
+++ TestFile.java
@@ -1,4 +1,15 @@
+/**
+ * TestFile
+ *
+ * @author Jane Doe 1234567 Group 42h
+ * @author John Doe 1234567 Group 42h
+ */
 class TestFile {
+	/**
+	 * Main method
+	 *
+	 * @param args foo
+	 */
 	public static void main(String[] args) {
 	}
 }
