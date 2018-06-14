dd76e3a65a4b0dbe3afd1e2687276554b03a11d5
Tim Schubert <tim.schubert@tu-bs.de>
2018-06-14 14:32:29 +0200

    fixup: escape

--- checkstyle.sh
+++ checkstyle.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 find / -name *.java
-java -jar ./checkstyle.jar -c /checkstyle/tubs_checks.xml $(find /builds/ -name '*.java')
+java -jar ./checkstyle.jar -c /checkstyle/tubs_checks.xml "$(find /builds/ -name '*.java')"
